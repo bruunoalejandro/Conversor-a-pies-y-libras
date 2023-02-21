409fc649d260b9db7ceed99bd9f75377a90d003b
Bruno Fernandes <125995286+bruunoalejandro@users.noreply.github.com>
2023-02-21 14:56:31 -0300

    Add files via upload

---
+++ pies_y_libras.py
@@ -0,0 +1,8 @@
+print("Ingrese la longitud del objeto: ")
+long = float(input())
+print("Ingrese el peso del objeto: ")
+peso = float(input())
+metro = long * 0.3048
+km = peso * 0.45359
+print("La longitud del objeto en metro es: ", metro)
+print("El peso del objeto en kilos es: ", km)
